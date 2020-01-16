86ecdf77f935b48eb38bd187829410ef3b3c9e22
Mike <mike@mikesantifort.nl>
2020-01-16 09:52:48 +0100

    My first attempt at sending braking commands.

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -128,9 +128,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneBP = [0.]
       ret.longitudinalTuning.deadzoneV = [0.]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.6, 1.5, 0.8]
+      ret.longitudinalTuning.kpV = [1.9, 1.4, 0.8]
       ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.42, 0.30]
+      ret.longitudinalTuning.kiV = [0.38, 0.28]
 
       ret.stoppingControl = True
       ret.directAccelControl = False
